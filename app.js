ac57c15b9a1571aaf4da44a72afc1db552aed2a5
Emir Rodzi <emir.f.mohd.rodzi@vanderbilt.edu>
2019-03-06 18:46:29 -0500

    Updated app.js for heroku deploy

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+(require('dotenv').config({ silent: process.env.NODE_ENV === 'production' }));
 
 const createError = require('http-errors');
 const express = require('express');
